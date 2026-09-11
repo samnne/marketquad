62d3858159900d53a8a8143a1c67111951968a25
Samuel Echefu <mashce@uvic.ca>
2026-09-10 23:45:41 -0700

    i think this will allow a third user

--- utils/functions.ts
+++ utils/functions.ts
@@ -29,7 +29,7 @@ export function cleanUP(
 
 export function matchUVIC(email: string) {
   const testerEmail = process.env.EXPO_PUBLIC_EMAIL_TESTER;
-  if (email === testerEmail) {
+  if (email === testerEmail || process.env.EXPO_PUBLIC_EMAIL_TESTER_2) {
     return true;
   }
   return email.includes("@uvic");
